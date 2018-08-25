e10acc57e9371e25371bd69f887e183b72300acb
hisuica <40780830+hisuica@users.noreply.github.com>
2018-08-25 20:54:59 +0900

    test commit

--- devlog/devlog/urls.py
+++ devlog/devlog/urls.py
@@ -18,6 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^blog/', include('blog.urls')),
-    url(r'^write/', include('write.urls')),
     url(r'^admin/', admin.site.urls),
 ]
